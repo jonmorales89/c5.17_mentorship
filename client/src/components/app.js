ad499bba3899e4a6fe35916ec00358e3aa4eb485
insoo lee <is1224lee@g.ucla.edu>
2017-07-31 15:42:38 -0700

    login_page

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './app.css';
 
 
-import logo from './imgs/logo.svg';
 import Navbar from './navbar';
+import LogIn from './logIn';
 import Footer from './footer';
 
 const App = () => (
     <div>
-    		<Navbar />
-        <div className="app">
-            <img src={logo} className="rotate"/>
-            <h1>Welcome to React!</h1>
-        </div>
+        <Navbar />
+        <LogIn />
         <Footer />
     </div>
 );
