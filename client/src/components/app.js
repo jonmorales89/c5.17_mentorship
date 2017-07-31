297e7e16e4e860607d374165bb0887a1f5a621fb
kieran jones <Kieran Jones>
2017-07-31 16:28:24 -0700

    mentee list item dashboard

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./home";
 import { Route, Link } from "react-router-dom";
+import DashBoard from './dashboard.js';
 
 const App = () =>
 	<div>
-		<Route path="/" component={Home} />
+		<Route exact path="/" component={Home} />
+		<Route exact path="/dashboard" component={DashBoard} />
 	</div>;
 
 export default App;
