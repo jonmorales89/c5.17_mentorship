ea495538599adcf4e20e5fa4b15da31dd23c369f
Dennis Dang <dangggdennis@gmail.com>
2017-07-31 16:13:48 -0700

    update app routes + navbar

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,10 +1,14 @@
-import React from "react";
-import Home from "./home";
-import { Route, Link } from "react-router-dom";
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
 
 const App = () =>
 	<div>
-		<Route path="/" component={Home} />
+		<Switch>
+			<Route path="/login" component={Login} />
+			<Route path="/" component={Home} />
+		</Switch>
 	</div>;
 
 export default App;
