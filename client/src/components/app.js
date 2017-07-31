d3d57f2dab0d430f01997efec93c4d241cfbbd96
Dennis Dang <dangggdennis@gmail.com>
2017-07-31 10:57:24 -0700

    firebase, navbar

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './app.css';
 import logo from './imgs/logo.svg';
+import Navbar from './navbar.js';
 
 const App = () => (
     <div>
+    		<Navbar />
         <div className="app">
             <img src={logo} className="rotate"/>
             <h1>Welcome to React!</h1>
