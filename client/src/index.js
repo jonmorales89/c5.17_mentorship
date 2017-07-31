74233be11b8ba0a83221cb64f31711372ad4e48c
Dennis Dang <dangggdennis@gmail.com>
2017-07-31 16:08:55 -0700

    prepared redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import promise from "redux-promise";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/index";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+// import thunk from "redux-thunk";
+import rootReducer from './reducers/index';
 
-import App from "./components/app";
+import App from './components/app';
 
-const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(rootReducer)}>
@@ -17,5 +17,5 @@ ReactDOM.render(
 			<App />
 		</Router>
 	</Provider>,
-	document.getElementById("root")
+	document.getElementById('root')
 );
--- client/src/reducers/mentor_reducer.js
+++ client/src/reducers/mentor_reducer.js
@@ -1,9 +1,11 @@
-import types from "../actions/types";
-const DEFAULT_STATE = { mentors: {} };
+import types from '../actions/types';
+const DEFAULT_STATE = { mentors: [] };
 
 export default function(state = DEFAULT_STATE, action) {
 	switch (action.type) {
 		case types.GET_MENTORS:
+			return state;
+		default:
 			return state;
 	}
 }
