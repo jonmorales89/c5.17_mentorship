4107fb8dba648d60248ad46c157d1295e0374f48
SunooBertsch <sbertsch92@yahoo.com>
2017-08-02 16:50:12 -0700

    cleaner tree and added search bar

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './app.css';
-
-import NavbarHome from './navbarHome';
+import Navbar from './navbar';
 import Footer from './footer';
 import Banner from './home_banner';
 
 const Home = () => {
 	return (
 		<div>
-			<NavbarHome />
+			<Navbar />
 			<Banner />
 			<Footer />
 		</div>
@@ -16,4 +15,3 @@ const Home = () => {
 };
 
 export default Home;
-
--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -1,90 +1,30 @@
-import React, { Component } from 'react';
-import Autosuggest from 'react-autosuggest';
+import React, {Component} from 'react';
 
-const styles = [
-    {
-        name: '91765'
-    },
-    {
-        name: '91766'
-    },
-    {
-        name: '91789'
-    },
-    {
-        name: '91744'
-    },
-    {
-        name: '91790'
-    },
-    {
-        name: '91734'
-    }
-];
+class Search extends Component{
+  constructor(props){
+    super(props);
+    this.state={value: ''};
 
-const getSuggestions = value => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+  handleSubmit(event){
+    event.preventDefault();
+  }
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Zipcode:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit"  />
+      </form>
+    );
+  }
 
-    return inputLength === 0 ? [] : styles.filter( style => style.name.toLowerCase().slice(0, inputLength) === inputValue)
-};
-
-const getSuggestionValue = suggestion => suggestion.name;
-
-const renderSuggestion = suggestion => (
-    <div>
-        {suggestion.name}
-    </div>
-);
-
-class Example extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            value: '',
-            suggestions: []
-        };
-    }
-
-    onChange = (event, { newValue }) => {
-        this.setState({
-            value: newValue
-        });
-    };
-
-    onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            suggestions: getSuggestions(value)
-        });
-    };
-
-    onSuggestionsClearRequested = () => {
-        this.setState({
-            suggestions: []
-        })
-    };
-
-    render() {
-        const { value, suggestions } = this.state;
-
-        const inputProps = {
-            palceholder: 'Type a dance style',
-            value,
-            onChange: this.onChange
-        };
-
-        return (
-            <Autosuggest
-                suggestions={suggestions}
-                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                getSuggestionValue={getSuggestionValue}
-                renderSuggestion={renderSuggestion}
-                inputProps={inputProps}
-            />
-        );
-    }
 }
-
-export default Example;
+export default Search
