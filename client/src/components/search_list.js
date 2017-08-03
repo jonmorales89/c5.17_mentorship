9e9332bda89c30397a374c96df4fe24f67d39a7a
jonmorales89 <jon.sal.morales@gmail.com>
2017-08-03 16:14:02 -0700

    fixed merge issues

--- client/src/components/search_list.js
+++ client/src/components/search_list.js
@@ -15,8 +15,6 @@ export default class SearchList extends Component {
       this.setState({ data });
     });
   }
-<<<<<<< HEAD
-
   charLimit(value){
     for(let i=0; i < value.length; i++){
       if(value.length > 80) {
@@ -27,12 +25,7 @@ export default class SearchList extends Component {
       }
     }
   }
-
-
-  render() {
-=======
   renderList(){
->>>>>>> d91f395f6bdbafaf89d989745907a3b315070768
     const { data } = this.state;
     const list = Object.keys(data).map((key, index) => {
       const path = window.location.pathname;
@@ -40,28 +33,6 @@ export default class SearchList extends Component {
       console.log("Location: ",loc);
       if(data[key].bio.location.split(" ").join("") === loc){
       return (
-<<<<<<< HEAD
-        <div key={index} className="col-xs-4">
-          <div className="card mr-3 mb-3" style={{ width: '20rem' }}>
-            <img className="card-img-top" src="http://lorempixel.com/output/people-q-c-318-180-1.jpg" alt="Card image cap" />
-            <div className="card-block">
-              <h5 className="card-title">
-                <div>{data[key].name}</div>
-              </h5>
-              <div className="card-text">
-                <div><strong>About Mentor:</strong></div>
-                <div>{this.charLimit(data[key].bio.aboutme)}</div>
-                <div className="text-capitalize"><strong>Dance Style:</strong> {data[key].bio.style}</div>
-                <div><strong>Location:</strong> {data[key].bio.location}</div>
-              </div>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-      );
-=======
           <li className="list-group-item" key={index}>
             <span>Name: {data[key].name}<br/></span>
             <span>About Me: {data[key].bio.aboutme}<br/></span>
@@ -72,7 +43,6 @@ export default class SearchList extends Component {
           </li>
         )
       }
->>>>>>> d91f395f6bdbafaf89d989745907a3b315070768
     });
     return list
   }
@@ -80,15 +50,9 @@ export default class SearchList extends Component {
     return (
       <div>
         <Navbar />
-<<<<<<< HEAD
-        <div className="row">
-            {list}
-        </div>
-=======
         <ul className="list-group">
           {this.renderList()}
         </ul>
->>>>>>> d91f395f6bdbafaf89d989745907a3b315070768
       </div>
     );
   }
