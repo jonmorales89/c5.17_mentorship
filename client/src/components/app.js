f822d988829e8b9a800146a65bfa89f565af1a1c
Dennis Dang <dangggdennis@gmail.com>
2017-08-03 11:05:27 -0700

    firebase authentication

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -6,30 +6,17 @@ import Home from './home';
 import Search from './search_list_item';
 import Login from './login';
 import Add from './tempAdd';
-<<<<<<< HEAD
 import MentorsSignUp from './mentor_signup_form';
+import FBLogin from './facebook_login';
 
 const App = () =>
 	<div>
+		<Route path="/login/facebook" component={FBLogin} />
 		<Route path="/mentors/signup" component={MentorsSignUp} />
 		<Route path="/add" component={Add} />
-		<Route path="/login" component={Login} />
+		<Route exact path="/login" component={Login} />
 		<Route path="/results" component={Search} />
 		<Route exact path="/" component={Home} />
-=======
-import MentorSignUp from './mentor_signup';
-
-const App = () =>
-	<div>
-		<Switch>
-			<Route path="/add" component={Add} />
-			<Route path="/login" component={Login} />
-			<Route path="/signup" component={MentorSignUp} />
-			<Route path="/results" component={Search} />
-			<Route path="/dashboard" component={DashBoard} />
-			<Route path="/" component={Home} />
-		</Switch>
->>>>>>> acb6651d8ca1ca340dd3f21ccc92fb42196ffed8
 	</div>;
 
 export default App;
