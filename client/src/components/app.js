c4f3d22140b563b232e8b02099df93259c0d5f4b
Dennis Dang <dangggdennis@gmail.com>
2017-08-02 18:24:26 -0700

    mentor signup form complete

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import Home from './home';
-import Search from "./search_list_item";
+import Search from './search_list_item';
 import Login from './login';
 import Add from './tempAdd';
+import MentorsSignUp from './mentor_signup_form';
 
 const App = () =>
 	<div>
-		<Switch>
-			<Route path="/add" component={Add} />
-			<Route path="/login" component={Login} />
-      		<Route path="/results" component={Search} />
-			<Route path="/" component={Home} />
-		</Switch>
+		<Route path="/mentors/signup" component={MentorsSignUp} />
+		<Route path="/add" component={Add} />
+		<Route path="/login" component={Login} />
+		<Route path="/results" component={Search} />
+		<Route exact path="/" component={Home} />
 	</div>;
 
 export default App;
