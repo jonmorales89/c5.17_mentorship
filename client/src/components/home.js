623a09308037e06f9b7dfc19a50e9c16b5ac9a9c
insoo lee <is1224lee@g.ucla.edu>
2017-08-03 15:19:15 -0700

    testing

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -5,12 +5,14 @@ import './app.css';
 import NavbarHome from './navbarHome';
 import Footer from './footer';
 import Banner from './home_banner';
+import Profile from './profile_card'
 
 const Home = () => {
 	return (
 		<div>
 			<NavbarHome />
 			<Banner />
+			<Profile />
 			<Footer />
 		</div>
 	);
