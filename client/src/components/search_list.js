80867eef8c5e433cbe968ad583b4e1cb1dd17901
kieran jones <Kieran Jones>
2017-08-07 16:36:55 -0700

    stuff

--- client/src/components/search_list.js
+++ client/src/components/search_list.js
@@ -156,6 +156,7 @@ export default class SearchList extends Component {
       <div>
         <div className="container">
           <div className="row" />
+          {list}
         </div>
       </div>
     );
