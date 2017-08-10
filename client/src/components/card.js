12d5e4f70b391b5ec469d5be66cd9e284e9de9c3
jonmorales89 <jon.sal.morales@gmail.com>
2017-08-09 18:19:41 -0700

    styled search list - primary visuals completed

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -17,45 +17,46 @@ class Card extends Component {
     }
 
     render() {
-        const { data, charLimit, dist } = this.props;
+        const { data, charLimit, affiliateLimit ,dist } = this.props;
 
         return (
             <div
                 onClick={() => this.toggleModal()}
-                className="col-xs-4"
+                className="mdl-cell mdl-cell--4-col mdl-card mdl-shadow--2dp"
                 dist={dist}
             >
-                <div className="card" style={{ width: '20rem' }}>
-                    <img
-                        className="card-img-top"
-                        src="https://dummyimage.com/318X180/b3b3b3/fff.png"
-                    />
-                    <div className="card-block">
-                        <h6 className="card-title">
-                            Name: {data.name}
+                    <div className="mdl-card__title mdl-card--expand">
+                        <h6 className="mdl-card__title-text">
+                            {data.name}
                         </h6>
-                        <div className="card-text">
-                            <p>About Me:</p>
-                            <p>
-                                {charLimit(data.bio.aboutme)}
-                            </p>
-                            <p>
-                                Affiliates: {data.bio.affiliates}
-                            </p>
-                            <p>
-                                Serving Location: {data.bio.location}
-                            </p>
-                        </div>
                     </div>
-                </div>
-                <SearchModal
+                    <div className="mdl-card__supporting-text">
+                        <div className="bold">About Me:</div>
+                       <div>
+                          {charLimit(data.bio.aboutme)}
+                       </div>
+                       <div>
+                           <div className="bold">Affiliates:</div>
+                           <span className="mdl-chip">
+                                <span className="mdl-chip__text">{affiliateLimit(data.bio.affiliates)}</span>
+                           </span>
+                       </div>
+                       <div>
+                           <div className="bold">Serving Location:</div>
+                           <div>{data.bio.location}</div>
+                       </div>
+                    </div>
+                    <div className="mdl-card__actions mdl-card--border">
+                        <button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Read More</button>
+                    </div>
+            <SearchModal
                     name={data.name}
-                    aboutme={charLimit(data.bio.aboutme)}
+                    aboutme={data.bio.aboutme}
                     affiliates={data.bio.affiliates}
                     serving={data.bio.location}
                     showModal={this.state.showModal}
                     toggleModal={() => this.toggleModal()}
-                />
+            />
             </div>
         );
     }
