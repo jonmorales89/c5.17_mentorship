962d08c8f5d13193f05f3abccdeb83b663e38b4c
insoo lee <is1224lee@g.ucla.edu>
2017-08-09 18:04:31 -0700

    update

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -4,36 +4,9 @@ import './css/bannerStyle.css';
 
 export default () => {
 	return (
-		<div>
+		<div className="body">
 			<SearchBar />
-			<div
-				id="carouselExampleSlidesOnly"
-				className="carousel slide"
-				data-ride="carousel">
-				<div className="blur carousel-inner" role="listbox">
-					<div className="carousel-item active">
-						<img
-							className="d-block2 img-fluid"
-							src="./dansu1.jpg"
-							alt="First slide"
-						/>
-					</div>
-					<div className="carousel-item">
-						<img
-							className="d-block2 img-fluid"
-							src="./dansu2.jpg"
-							alt="Second slide"
-						/>
-					</div>
-					<div className="carousel-item">
-						<img
-							className="d-block2 img-fluid"
-							src="./dansu3.jpg"
-							alt="Third slide"
-						/>
-					</div>
-				</div>
-			</div>
+			<img src="./dansu1.jpg" alt="banner picture"/>
 		</div>
 	);
 };
