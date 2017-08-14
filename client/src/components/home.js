e8384217b91a955ea615547d98bb7c9a94a2c0ba
insoo lee <is1224lee@g.ucla.edu>
2017-08-14 12:01:07 -0700

    merging_insoo

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -18,24 +18,3 @@ export default () => {
 		</div>
 	);
 };
-
-
-
-
-
-
-		{/*<div className="homeBanner">*/}
-			{/*<img*/}
-				{/*src="./dansu1.png"*/}
-				{/*alt="banner picture"*/}
-				{/*className="img-responsive homePhoto"*/}
-			{/*/>*/}
-			{/*<div className="text-center mx-5">*/}
-				{/*<h4>Live 1:1 Q&A from industry and community dancers</h4>*/}
-				{/*<h5>*/}
-					{/*Dansumentor is your live 1:1 expert dance mentor helping you in real*/}
-					{/*time*/}
-				{/*</h5>*/}
-			{/*</div>*/}
-			{/*<SearchBar className="text-center" InputClassName="" />*/}
-		{/*</div>*/}
