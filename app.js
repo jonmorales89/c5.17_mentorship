58dbd79f697a06fd137fa995ae57dc484ac37be1
root <root@ip-172-31-32-24.us-east-2.compute.internal>
2017-08-18 23:03:45 +0000

    included cors and nodemailer works

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
+
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
