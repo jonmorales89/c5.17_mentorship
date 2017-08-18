e8f44fe5425b50ad1975b724c19a0e1d03dbc6c4
Dennis Dang <dangggdennis@gmail.com>
2017-08-18 15:44:13 -0700

    added code for bodyparser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
